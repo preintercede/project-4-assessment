a54bc19e60de547493f247e5710b631e6d44351a
Dylan <email@example.com>
2020-06-02 14:02:52 -0700

    Updates

---
+++ precache-manifest.8b87a96622632e8e75bcd18740e623be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273b93325c5e4f27ae57bd453db13f02",
+    "url": "/project4-assessment/index.html"
+  },
+  {
+    "revision": "1393ccfe77936bbe4ad1",
+    "url": "/project4-assessment/static/css/main.89055b15.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1393ccfe77936bbe4ad1",
+    "url": "/project4-assessment/static/js/main.c6acc5b0.chunk.js"
+  },
+  {
+    "revision": "2a6b3f220a2f998b9f47",
+    "url": "/project4-assessment/static/js/runtime-main.96fb3b05.js"
+  }
+]);
